f80ef01b875efb9e69a0d23448a520ccca381981
Razvan <rpaduroiu@gmail.com>
2019-10-13 05:27:05 +0200

    edit and delete articles

--- articles/urls.py
+++ articles/urls.py
@@ -7,5 +7,7 @@ app_name = "articles"
 urlpatterns = [
     path('', views.article_list, name="list"),
     url(r'^create/$', views.article_create, name="create"),
+    url(r'^edit/(?P<slug>[\w-]+)/$', views.article_edit, name="edit"),
+    url(r'^delete/(?P<slug>[\w-]+)/$', views.article_delete, name="delete"),
     url(r'^(?P<slug>[\w-]+)/$', views.article_detail, name="detail")
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Article
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404;
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFound, HttpResponseForbidden;
 from django.contrib.auth.decorators import login_required
 # for login
 from django.contrib.auth.decorators import login_required
@@ -34,3 +35,34 @@ def article_create(request):
     else:
         form = forms.CreateArticle()
     return render(request, 'articles/article_create.html', {'form': form})
+
+@login_required(login_url='/loginapp/login/')
+def article_edit(request, slug=None):
+    if slug:
+        article = get_object_or_404(Article, slug=slug)
+        if article.author != request.user:
+            return HttpResponseForbidden()
+    else:
+        return HttpResponseBadRequest()
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST or None, instance=article)
+        if form.is_valid():
+            # save article to db
+            form.save()
+            return redirect('articles:list')
+    else:
+        form = forms.CreateArticle(instance=article)
+        return render(request, 'articles/article_edit.html', {'form': form, 'article': article})
+
+@login_required(login_url='/loginapp/login/')
+def article_delete(request, slug=None):
+    if slug:
+        article = get_object_or_404(Article, slug=slug)
+        if article.author != request.user:
+            return HttpResponseForbidden()
+    else:
+        return HttpResponseBadRequest()
+    if request.method == 'POST':
+        article.delete()
+
+    return redirect('articles:list')
